c0855cbcb430c4897ca2e275a4d5aa58d05b8d37
K. Singh <16361374+yourskiss@users.noreply.github.com>
2022-07-06 10:47:42 +0530

    ui

---
+++ scripts/common.js
@@ -0,0 +1,101 @@
+/* page refresh on orientation change === START */
+$(window).on('orientationchange', function (event) 
+{
+    location.reload(true);
+});
+/* page refresh on orientation change === END */
+
+
+/* onLoad === start */
+$(window).on('load', function() 
+{
+     
+});
+/* onLoad === end */
+
+$(".navigation .wrapper ul li.megamenu a").click(function()
+{
+    if($(this).parent().hasClass('active'))
+    {
+        $(this).next(".dropdownmenu").slideUp(400);
+        $(this).parent().removeClass("active");
+        $(this).find("i.fa").removeClass("fa-angle-up").addClass("fa-angle-down");
+        $("body").css('overflow','auto');
+    }
+    else
+    {
+        $(".dropdownmenu").slideUp(400);
+        $(".navigation .wrapper ul li.megamenu").removeClass("active");
+        $(".navigation .wrapper ul li.megamenu a i.fa").removeClass("fa-angle-up").addClass("fa-angle-down");
+        $(this).next(".dropdownmenu").slideDown(400);
+        $(this).parent().addClass("active");
+        $(this).find("i.fa").removeClass("fa-angle-down").addClass("fa-angle-up");
+        $("body").css('overflow','hidden');
+    }
+});
+
+$(".dropdownclose").click(function()
+{
+    $(".dropdownmenu").slideUp(400);
+    $(".navigation .wrapper ul li.megamenu").removeClass("active");
+    $(".navigation .wrapper ul li.megamenu a i.fa").removeClass("fa-angle-up").addClass("fa-angle-down");
+    $("body").css('overflow','auto');
+});
+
+
+
+/* ############################ sliders === start ############################ */
+$(function($) 
+{
+    if($("#bannerhome").length > 0)
+    {
+        $('.bannerhomeslider').slick({
+            slidesToShow:1,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            infinite: false,
+            adaptiveHeight: false,
+            centerMode: false,
+            centerPadding: '0',
+            initialSlide:0,
+            responsive:
+            [
+                {
+                    breakpoint: 767,
+                    settings: { arrows: true  }
+                }
+            ]
+        });
+    }
+
+    if($(".knowledgeresources").length > 0)
+    {
+        $('.knowledgeresourcesslider').slick({
+            slidesToShow:3,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            infinite: false,
+            adaptiveHeight: false,
+            centerMode: false,
+            centerPadding: '0',
+            initialSlide:0,
+            responsive:
+            [
+                {
+                    breakpoint: 767,
+                    settings: { arrows: true  }
+                }
+            ]
+        });
+    }
+
+    
+
+});
+/* ############################ sliders === end ############################ */
